0a6986948d7e1adafe8bdf05e27ad5a10b8f016a
Adamozo <naworsk.adam@gmail.com>
2022-03-12 19:41:24 +0100

    few password from_str fixes

--- src/ex2.rs
+++ src/ex2.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 use std::num::ParseIntError;
-use regex::Regex;
+use std::string::ParseError;
+use lazy_regex::regex;
 
 pub struct Password{
     min_number: u16,
@@ -13,43 +14,48 @@ impl Password{
     pub fn is_valid(&self) -> bool {
         let counter: u16 = self.passwd.chars().into_iter().filter(|c| c == &self.checked_char).count().try_into().unwrap();
     
-        (self.min_number .. self.max_number+1).contains(&counter)
+        (self.min_number..=self.max_number).contains(&counter)
     } 
 
 }
+
+enum PasswordError{
+    ParseIntError,
+    ParseError,
+    Infallible
+}
+
 
 impl FromStr for Password{
     type Err = ParseIntError;
     // 1-3 a: abcde
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = regex!(r"(\d+)-(\d+) (\w{1}): (\w+)");
+        let caps = re.captures(s);
 
-        let val = Regex::new(r"(\d+)-(\d+) (\w{1}): (\w+)").unwrap();
+        match caps {
+            Some(r) => {        
+                let min = r.get(1).map_or("", |m| m.as_str()).parse::<u16>()?;
+                let max = r.get(2).map_or("", |m| m.as_str()).parse::<u16>()?;
+                let c = match r.get(3).map_or("", |m| m.as_str()).chars().next() {
+                    Some(r) => r,
+                    None => panic!()
+                };
 
-        match val.is_match(s)  {
-            true => {
-                let coords: Vec<&str> = s.split(' ').collect();
-                let ranges: Vec<&str> = coords[0].split("-").collect();
-                        
-                let min_number = ranges[0].parse::<u16>()?;
-                let max_number = ranges[1].parse::<u16>()?;
-                let temp: Vec<char> = coords[1].chars().collect();
+                let password = r.get(4).map_or("", |m| m.as_str());
 
-                Ok(Password { min_number: min_number, max_number: max_number, checked_char: temp[0],  passwd: coords[2].to_string()})
-            }
-
-            false => {
-                Ok(Password {min_number: s.parse::<u16>().unwrap(), max_number: s.parse::<u16>().unwrap(), checked_char: 'e', passwd: "error".to_string()})
-            }
-        }
-        
-        
+                Ok(Password { min_number: min, max_number: max, checked_char: c,  passwd: password.to_string()})
+            },
+            None => panic!()
+        }        
     }
 }
 
 pub fn run() {
-    let p1 = Password::from_str("1-3 a: abcde").unwrap();
-    assert_eq!(true, Password::is_valid(&p1));
-
-    let p2 = Password::from_str("2-3 a: abcde").unwrap();
-    assert_eq!(false, Password::is_valid(&p2));
+    let p1 = Password::from_str("1--3 a: abcde");
+    match p1 {
+        Ok(r) => println!("{:?} {:?} {:?} {:?}", r.min_number, r.max_number, r.checked_char, r.passwd),
+        Err(r) => println!(" error {:?}", r),
+        _ => println!("missmatch")
+    }       
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ mod tests {
     use crate::ex2::Password;
     use std::str::FromStr;
 
-    #[test_case("1-3 a: abcde", true ; "when both operands are negative")]
-    #[test_case("1-3 b: cdefg",  false  ; "when both operands are positive")]
-    #[test_case("2-9 c: ccccccccc",  true  ; "when operands are swapped")]
+    #[test_case("1-3 a: abcde", true ; "valid 1-3 a: abcde")]
+    #[test_case("1-3 b: cdefg",  false  ; "invalid 1-3 b: cdefg")]
+    #[test_case("2-9 c: ccccccccc",  true  ; "valid 2-9 c: ccccccccc")]
+    #[test_case("3-1 a: abcde", false ; "valid 3-1 a: abcde")]
     fn is_valid_test(s: &str, res: bool){
         let p1 = Password::from_str(s).unwrap();
         assert_eq!(res, Password::is_valid(&p1));
