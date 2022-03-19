c9e2895c4a4db3e0c219ab6a709cb9dd7c88e182
Adamozo <naworsk.adam@gmail.com>
2022-03-19 20:35:20 +0100

    working ex2

--- src/ex2.rs
+++ src/ex2.rs
@@ -1,7 +1,8 @@
 use lazy_regex::{regex, Lazy, Regex};
 use std::str::FromStr;
-use test_case::test_case;
 
+#[derive(PartialEq)]
+#[derive(Debug)]
 pub struct Password {
     min_number: u16,
     max_number: u16,
@@ -56,3 +57,30 @@ pub fn run(){
         _ => println!(" error")
      }
  }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("1-3 a: abcde" => Ok(Password{min_number: 1, max_number: 3, checked_char: 'a', passwd: "abcde".to_string()}); "valid webiste 1")]
+    #[test_case("1-3 b: cdefg" => Ok(Password{min_number: 1, max_number: 3, checked_char: 'b', passwd: "cdefg".to_string()}); "valid website 2")]
+    #[test_case("2-9 c: ccccccccc" => Ok(Password{min_number: 2, max_number: 9, checked_char: 'c', passwd: "ccccccccc".to_string()}); "valid website 3")]
+    #[test_case("1-c a: abcde" => Err("unable to capture".to_string()); "invalid letter as max_number")]
+    #[test_case("c-3 a: abcde" => Err("unable to capture".to_string()); "invalid letter as min_number")]
+    #[test_case("1-3 1: abcde" => Ok(Password{min_number: 1, max_number: 3, checked_char: '1', passwd: "abcde".to_string()}); "valid num as checked char")]
+    #[test_case("1-3 a: " => Err("unable to capture".to_string()); "invalid lack of password")]
+    fn test_from_str(s: &str) -> Result<Password, String>{
+        Password::from_str(s)
+    }
+
+
+    #[test_case("1-3 a: abcde" => true ; "valid 1-3 a: abcde")]
+    #[test_case("1-3 b: cdefg" =>  false  ; "invalid 1-3 b: cdefg")]
+    #[test_case("2-9 c: ccccccccc" =>  true  ; "valid 2-9 c: ccccccccc")]
+    #[test_case("3-1 a: abcde" => false ; "invalid 3-1 a: abcde")]
+    fn test_is_valid(s: &str) -> bool{
+        let p1 = Password::from_str(s).unwrap();
+        Password::is_valid(&p1)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,11 @@
+mod ex2;
 //mod ex1;
-mod ex1;
 // use std::process;
-
-// #[cfg(test)]
-// mod tests {
-//     use crate::ex2::Password;
-//     use std::str::FromStr;
-//     use test_case::test_case;
-
-//     #[test_case("1-3 a: abcde", true ; "valid 1-3 a: abcde")]
-//     #[test_case("1-3 b: cdefg",  false  ; "invalid 1-3 b: cdefg")]
-//     #[test_case("2-9 c: ccccccccc",  true  ; "valid 2-9 c: ccccccccc")]
-//     #[test_case("3-1 a: abcde", false ; "valid 3-1 a: abcde")]
-//     fn is_valid_test(s: &str, res: bool) {
-//         let p1 = Password::from_str(s).unwrap();
-//         assert_eq!(res, Password::is_valid(&p1));
-//     }
-// }
 
 fn main() {
     // if let Err(err) = ex2::run() {
     //     eprintln!("Error: {:?}", err);
     //     process::exit(1);
     // }
-
-    //ex1
-     let nums: [u32; 6] = [1721, 979, 366, 299, 675, 1456];
-     let sum: u32 = 2020;
-
-     let res: Vec<(u32, u32)> = ex1::check_sum(&nums, &sum);
-     println!("Nums: {:?}", &nums);
-     println!("Expected sum: {:?}", &sum);
-     println!("Result: {:?}", &res);
-
     
 }
