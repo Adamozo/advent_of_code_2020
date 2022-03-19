4d03dcd6d29b871d33a85784f6f81b88f816146c
Adamozo <naworsk.adam@gmail.com>
2022-03-19 15:03:25 +0100

    run of ex1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
-//mod ex1;
-mod ex2;
-
-#[cfg(test)]
-mod tests {
-    use test_case::test_case;
-    use crate::ex2::Password;
-    use std::str::FromStr;
-
-    #[test_case("1-3 a: abcde", true ; "when both operands are negative")]
-    #[test_case("1-3 b: cdefg",  false  ; "when both operands are positive")]
-    #[test_case("2-9 c: ccccccccc",  true  ; "when operands are swapped")]
-    fn is_valid_test(s: &str, res: bool){
-        let p1 = Password::from_str(s).unwrap();
-        assert_eq!(res, Password::is_valid(&p1));
-    }
-}
+mod ex1;
 
 fn main() {
-    ex2::run();
+    println!("{:?}", ex1::check_sum(&[1721, 979, 366, 299, 675, 1456], &2020));
 }
