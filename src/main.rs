8fc0f01c0d6e4297c7edbe2899dcb443c6bb3d99
Adamozo <naworsk.adam@gmail.com>
2022-03-25 21:26:52 +0100

    main edited

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-//mod ex1;
 mod ex2;
+//mod ex1;
+use anyhow::{Context, Result};
 
-#[cfg(test)]
-mod tests {
-    use crate::ex2::Password;
-    use std::str::FromStr;
-    use test_case::test_case;
-
-    #[test_case("1-3 a: abcde", true ; "when both operands are negative")]
-    #[test_case("1-3 b: cdefg",  false  ; "when both operands are positive")]
-    #[test_case("2-9 c: ccccccccc",  true  ; "when operands are swapped")]
-    fn is_valid_test(s: &str, res: bool) {
-        let p1 = Password::from_str(s).unwrap();
-        assert_eq!(res, Password::is_valid(&p1));
-    }
-}
-
-fn main() {
-    ex2::run();
+fn main() -> Result<()>{
+    let filename = "data_files/ex2_passwords.txt";
+    ex2::run(filename).with_context(|| format!("Failed to read data from {}", filename))
 }
