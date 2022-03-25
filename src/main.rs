615f938f5e248f532f995bb0fd767da6f1a27d8a
Adamozo <naworsk.adam@gmail.com>
2022-03-25 20:47:06 +0100

    deleted rustfmt and formated code

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ mod ex2;
 
 #[cfg(test)]
 mod tests {
-    use test_case::test_case;
     use crate::ex2::Password;
     use std::str::FromStr;
+    use test_case::test_case;
 
     #[test_case("1-3 a: abcde", true ; "when both operands are negative")]
     #[test_case("1-3 b: cdefg",  false  ; "when both operands are positive")]
     #[test_case("2-9 c: ccccccccc",  true  ; "when operands are swapped")]
-    fn is_valid_test(s: &str, res: bool){
+    fn is_valid_test(s: &str, res: bool) {
         let p1 = Password::from_str(s).unwrap();
         assert_eq!(res, Password::is_valid(&p1));
     }
