6e2f81581464c8339fcd12f4ab14f106503f88ae
Adamozo <naworsk.adam@gmail.com>
2022-03-31 18:07:27 +0200

    added tests

--- src/ex4.rs
+++ src/ex4.rs
@@ -14,9 +14,8 @@ fn load_data<P>(path: P) -> io::Result<Vec<String>>
 where
     P: AsRef<Path>,
 {
-    let mut passports: Vec<String> = Vec::new();
-    passports.push("".to_owned());
-
+    let mut passports: Vec<String> = vec!["".to_owned()];
+    
     for line in read_lines(path)? {
         let line = line?;
         if line.is_empty() {
@@ -42,7 +41,7 @@ fn is_field_interesting(f: &str) -> bool {
     false
 }
 
-fn count_valid_passports(passports: &[String]) -> io::Result<usize> {
+fn count_valid_passports(passports: &[String]) -> usize {
     let res = passports
         .iter()
         .filter(|e| {
@@ -54,7 +53,7 @@ fn count_valid_passports(passports: &[String]) -> io::Result<usize> {
                 >= 7
         })
         .count();
-    Ok(res)
+    res
 }
 
 pub fn run<P>(path: P) -> io::Result<()>
@@ -62,7 +61,7 @@ where
     P: AsRef<Path>,
 {
     let passports = load_data(path)?;
-    println!("{:?}", count_valid_passports(&passports)?);
+    println!("{:?}", count_valid_passports(&passports));
 
     Ok(())
 }
@@ -70,38 +69,44 @@ where
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
-    // use test_case::test_case;
+    use super::*;
+    use test_case::test_case;
 
-    // #[test_case("1-3 a: abcde" => Ok(Password{min_number: 1, max_number: 3, checked_char: 'a', passwd: "abcde".to_string()}); "valid webiste 1")]
-    // #[test_case("1-3 b: cdefg" => Ok(Password{min_number: 1, max_number: 3, checked_char: 'b', passwd: "cdefg".to_string()}); "valid website 2")]
-    // #[test_case("2-9 c: ccccccccc" => Ok(Password{min_number: 2, max_number: 9, checked_char: 'c', passwd: "ccccccccc".to_string()}); "valid website 3")]
-    // #[test_case("1-c a: abcde" => Err(PasswordError::CaptureFailed); "invalid letter as max_number")]
-    // #[test_case("c-3 a: abcde" => Err(PasswordError::CaptureFailed); "invalid letter as min_number")]
-    // #[test_case("1-3 1: abcde" => Ok(Password{min_number: 1, max_number: 3, checked_char: '1', passwd: "abcde".to_string()}); "valid num as checked char")]
-    // #[test_case("1-3 a: " => Err(PasswordError::CaptureFailed); "invalid lack of password")]
-    // fn test_from_str(s: &str) -> Result<Password, PasswordError> {
-    //     s.parse::<Password>()
-    // }
+    #[test_case("aaa" => false ; "field not interesting")]
+    #[test_case("byr" => true ; "field byr interesting")]
+    #[test_case("iyr" => true ; "field iyr interesting")]
+    #[test_case("eyr" => true ; "field eyr interesting")]
+    #[test_case("hgt" => true ; "field hgt interesting")]
+    #[test_case("hcl" => true ; "field hcl interesting")]
+    #[test_case("ecl" => true ; "field ecl interesting")]
+    #[test_case("pid" => true ; "field pid interesting")]
+    fn test_ex4_is_field_interesting(f: &str) -> bool{
+        is_field_interesting(f)
+    }
 
-    // #[test_case("1-3 a: abcde" => Ok(true) ; "valid 1-3 a: abcde")]
-    // #[test_case("1-3 b: cdefg" =>  Ok(false)  ; "invalid 1-3 b: cdefg")]
-    // #[test_case("2-9 c: ccccccccc" =>  Ok(true)  ; "valid 2-9 c: ccccccccc")]
-    // #[test_case("3-1 a: abcde" => Ok(false) ; "invalid 3-1 a: abcde")]
-    // #[test_case("1-c a: abcde" => Err(PasswordError::CaptureFailed); "invalid letter as max_number")]
-    // #[test_case("c-3 a: abcde" => Err(PasswordError::CaptureFailed); "invalid letter as min_number")]
-    // fn test_is_valid(s: &str) -> Result<bool, PasswordError> {
-    //     let p1 = s.parse::<Password>()?;
-    //     Ok(p1.is_valid())
-    // }
+    #[test]
+    fn test_ex4_run_no_file() {
+        assert!(run("aaa").is_err())
+    }
 
-    // #[test]
-    // fn test_run_no_file() {
-    //     assert!(run("aaa").is_err())
-    // }
+    #[test]
+    fn test_run_file_exists() {
+        assert!(run("data_files/ex4.txt").is_err() == false)
+    }
 
-    // #[test]
-    // fn test_run_file_exists() {
-    //     assert!(run("data_files/ex2_passwords.txt").is_err() == false)
-    // }
+    #[test]
+    fn test_ex4_count_valid_passports(){
+        let valid: [String; 2] = ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm".to_string(),
+        "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm".to_string()];
+
+        assert_eq!(count_valid_passports(&valid), 2);
+    }
+
+    #[test]
+    fn test_ex4_count_invalid_passports(){
+        let invalid: [String; 2] = [
+         "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in".to_string(), "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929".to_string()];
+
+        assert_eq!(count_valid_passports(&invalid), 0);
+    }
 }
