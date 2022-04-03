6d7995f18875ea103b13e64d664a4cb0b016cb18
Adamozo <naworsk.adam@gmail.com>
2022-04-03 17:36:13 +0200

    working code

--- src/ex5.rs
+++ src/ex5.rs
@@ -1,6 +1,3 @@
-// wyciecie z wejscia pierwszych cześci od rzędów i części od kolumnt
-// przjsice po znakach i zmapowanie ich jako ciągu liczb binarnych
-
 /*
 
     BFFFBBFRRR: row 70, column 7, seat ID 567.
@@ -10,5 +7,30 @@
     row * 8 + column = seat id
 */
 
-// decode_id(code: &str) - function that takes code and returns struc seat with fields row, column, id
-// moża zrobić jednolinikowcami - slice, po nim iterator i mapownie na 0/1 albo map z foldem
+use std::io::{self};
+
+fn extract_seat_num(input: &str) -> (u32, u32, u32){
+    let mut column = 0;
+    let base: u32 = 2;
+    let mut row = 0;
+    for (index,p) in input.chars().enumerate(){
+        if p == 'B'{
+            row += base.pow(6-index as u32);
+        } 
+
+        else if p == 'R'{
+            column += base.pow(9-index as u32);
+        }
+    }
+
+    (column, row, row*8 + column)
+}
+
+pub fn run() -> io::Result<()>{
+
+    println!("FBFBBFFRLR: {:?}", extract_seat_num("FBFBBFFRLR"));
+    println!("BFFFBBFRRR: {:?}", extract_seat_num("BFFFBBFRRR"));
+    println!("FFFBBBFRRR: {:?}", extract_seat_num("FFFBBBFRRR"));
+    println!("BBFFBBFRLL: {:?}", extract_seat_num("BBFFBBFRLL"));
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use advent_of_code::*;
 
 fn main() -> Result<()>{
     let filename = "data_files/ex2_passwords.txt";
-    ex2::run(filename).with_context(|| format!("Failed to read data from {}", filename))
+    ex5::run().with_context(|| format!("Failed to read data from {}", filename))
 }
