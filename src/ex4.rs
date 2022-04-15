de16105b685fe3b408085de71fff2f52b7aab9d9
Adamozo <naworsk.adam@gmail.com>
2022-04-15 22:05:10 +0200

    rewrited ex4

--- src/ex4.rs
+++ src/ex4.rs
@@ -10,56 +10,54 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn get_index(filed: &str) -> i16
+{
+    match filed {
+        "byr" => 0, 
+        "iyr" => 1,
+        "eyr" => 2, 
+        "hgt" => 3, 
+        "hcl" => 4, 
+        "ecl" => 5, 
+        "pid" => 6,
+        _ => -1
+    }
+}
+
 fn count_valid_passports<P>(path: P) -> io::Result<usize>
 where
     P: AsRef<Path>,
 {
-    let mut container: String = String::new();
+    let mut mapper:[bool; 7] = [false, false, false, false, false, false, false];
     let mut counter: usize = 0;
 
-    let last_empty_string_iter = vec![Ok("".into())].into_iter();
-    for line in read_lines(path)?.chain(last_empty_string_iter) {
-
-    for line in read_lines(path)? {
+    for line in read_lines(path)?{
         let line = line?;
-        if line.is_empty() {
-            if is_passport_valid(&container){
+        if line.is_empty(){
+            if !mapper.iter_mut().any(|e| !(*e)){
                 counter += 1;
-                container = " ".to_owned();
             }
-        } else {
-            container += " ";
-            container += &*line;
+
+            mapper = [false, false, false, false, false, false, false];
         }
+
+        else{
+            for l in line.split(' '){
+                let to_check = l.split(':').next().unwrap();
+                let index = get_index(to_check);
+
+                if index != -1{
+                    mapper[index as usize] = !mapper[index as usize];
+                }
+            }
+        }
+    }
+
+    if !mapper.iter_mut().any(|e| !(*e)){
+        counter += 1;
     }
 
     Ok(counter)
-}
-
-fn is_field_interesting(f: &str) -> u32 {
-    let interesting_fields = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-    if !f.is_empty() {
-        let field = &f[0..3];  // split_once
-        if interesting_fields.iter().any(|f| *f == field) {
-            return 1;
-        }
-    }
-
-    0
-}
-
-fn is_passport_valid(passport: &str) -> bool {
-    let mut res = 0;
-    for p in passport.split(' '){
-        if is_field_interesting(&p) + res >= 7{
-            return true;
-        }
-
-        else if is_field_interesting(&p)==1{
-            res += 1;
-        }
-    }
-    false
 }
 
 pub fn run<P>(path: P) -> io::Result<()>
@@ -75,19 +73,6 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use test_case::test_case;
-
-    #[test_case("aaa" => 0 ; "field not interesting")]
-    #[test_case("byr" => 1 ; "field byr interesting")]
-    #[test_case("iyr" => 1 ; "field iyr interesting")]
-    #[test_case("eyr" => 1 ; "field eyr interesting")]
-    #[test_case("hgt" => 1 ; "field hgt interesting")]
-    #[test_case("hcl" => 1 ; "field hcl interesting")]
-    #[test_case("ecl" => 1 ; "field ecl interesting")]
-    #[test_case("pid" => 1 ; "field pid interesting")]
-    fn test_ex4_is_field_interesting(f: &str) -> u32 {
-        is_field_interesting(f)
-    }
 
     #[test]
     fn test_ex4_run_no_file() {
@@ -102,12 +87,5 @@ mod tests {
     #[test]
     fn test_ex4_count_valid_passports() {
         assert_eq!(count_valid_passports("data_files/ex4.txt").unwrap(), 2);
-    }
-
-    #[test_case("hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in" => false; "passport invalid")]
-    #[test_case("hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm" => true ; "passport valid")]
-    #[test_case("" => false ; " emptypassport invalid")]
-    fn test_ex4_is_passport_valid(passport: &str) -> bool{
-        is_passport_valid(passport)
     }
 }
