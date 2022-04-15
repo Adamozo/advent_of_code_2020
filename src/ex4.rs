68faa9e23920097ffcfd2e5784341bcdc3334572
Adamozo <naworsk.adam@gmail.com>
2022-04-15 22:09:00 +0200

    formated ex4

--- src/ex4.rs
+++ src/ex4.rs
@@ -10,17 +10,16 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn get_index(filed: &str) -> i16
-{
+fn get_index(filed: &str) -> i16 {
     match filed {
-        "byr" => 0, 
+        "byr" => 0,
         "iyr" => 1,
-        "eyr" => 2, 
-        "hgt" => 3, 
-        "hcl" => 4, 
-        "ecl" => 5, 
+        "eyr" => 2,
+        "hgt" => 3,
+        "hcl" => 4,
+        "ecl" => 5,
         "pid" => 6,
-        _ => -1
+        _ => -1,
     }
 }
 
@@ -28,32 +27,30 @@ fn count_valid_passports<P>(path: P) -> io::Result<usize>
 where
     P: AsRef<Path>,
 {
-    let mut mapper:[bool; 7] = [false, false, false, false, false, false, false];
+    let mut mapper: [bool; 7] = [false, false, false, false, false, false, false];
     let mut counter: usize = 0;
 
-    for line in read_lines(path)?{
+    for line in read_lines(path)? {
         let line = line?;
-        if line.is_empty(){
-            if !mapper.iter_mut().any(|e| !(*e)){
+        if line.is_empty() {
+            if !mapper.iter_mut().any(|e| !(*e)) {
                 counter += 1;
             }
 
             mapper = [false, false, false, false, false, false, false];
-        }
-
-        else{
-            for l in line.split(' '){
+        } else {
+            for l in line.split(' ') {
                 let to_check = l.split(':').next().unwrap();
                 let index = get_index(to_check);
 
-                if index != -1{
+                if index != -1 {
                     mapper[index as usize] = !mapper[index as usize];
                 }
             }
         }
     }
 
-    if !mapper.iter_mut().any(|e| !(*e)){
+    if !mapper.iter_mut().any(|e| !(*e)) {
         counter += 1;
     }
 
