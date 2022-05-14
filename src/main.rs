bb4ef381ad7f3f4ee5bbee4e9ea2c6773b76b898
Adamozo <naworsk.adam@gmail.com>
2022-05-14 12:55:09 +0200

    finished

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ use advent_of_code::*;
 use anyhow::{Context, Result};
 
 fn main() -> Result<()> {
-    let filename = "data_files/ex10.txt";
-    ex10::run(filename).with_context(|| format!("Failed to read data from {}", filename))
+    ex13::run().with_context(|| format!("Error in {}", "ex11.rs"))
 }
