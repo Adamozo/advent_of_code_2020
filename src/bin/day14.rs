9ab3ddfc6ef22996db37b6b9fed547a36261d19c
Adamozo <naworsk.adam@gmail.com>
2022-05-21 02:02:33 +0200

    few tests

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -7,9 +7,22 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+    use advent_of_code::ex14;
+    
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn ex14_process_mask() {
+        let result = ex14::process_mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X").unwrap();
+        assert_eq!((result.0, !result.1), (64, 2));
+    }
+
+    #[test]
+    fn ex14_process_input() {
+        use aoc_utils::read_to_string;
+        let dict: HashMap<u64, u64> = HashMap::from([
+            (7, 101),
+            (8, 0)
+        ]);
+        assert_eq!(ex14::prepare_input(&read_to_string("data_files/ex14.txt").unwrap()).unwrap(), (64, 2, dict));
     }
 }
