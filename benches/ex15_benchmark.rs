e830a532d80bc4060da770f8f7ac7a62d00f1f02
Adamozo <naworsk.adam@gmail.com>
2022-05-27 00:38:37 +0200

    benchamrks

---
+++ benches/ex15_benchmark.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate criterion;
+
+use advent_of_code::ex15::*;
+use advent_of_code::ex15b::*;
+use advent_of_code::ex15c::*;
+use aoc_utils::DaySolver;
+use criterion::Criterion;
+
+fn ex13_benchmark(c: &mut Criterion) {
+    c.bench_function("vector tuple", |b| {
+        b.iter(|| Day15VersionA::solve_default_file())
+    });
+}
+
+fn ex13_benchmark2(c: &mut Criterion) {
+    c.bench_function("hash map", |b| {
+        b.iter(|| Day15VersionB::solve_default_file())
+    });
+}
+
+fn ex13_benchmark3(c: &mut Criterion) {
+    c.bench_function("vector with capacity", |b| {
+        b.iter(|| Day15VersionC::solve_default_file())
+    });
+}
+
+criterion_group!(benches, ex13_benchmark, ex13_benchmark2, ex13_benchmark3);
+criterion_main!(benches);
